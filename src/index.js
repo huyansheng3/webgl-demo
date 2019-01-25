d05d93bbb39255d627ac45d3eb54a4c87853b414
huyansheng <huyansheng@didichuxing.com>
2019-01-25 15:23:54 +0800

    add colorful triangle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import { createProgram } from "./createProgram";
-import square_f from "./shaders/square_f.glsl";
-import square_v from "./shaders/square_v.glsl";
 import { mat4 } from "gl-matrix";
 import "./index.css";
 
@@ -22,7 +20,7 @@ function initBuffers(gl) {
 
   // Now create an array of positions for the square.
 
-  const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+  const positions = [-0.5, -0.5, 0.5, -0.5, 0, 0.5];
 
   // Now pass the list of positions into WebGL to build the
   // shape. We do this by creating a Float32Array from the
@@ -33,93 +31,6 @@ function initBuffers(gl) {
   return {
     position: positionBuffer
   };
-}
-
-//
-// Draw the scene.
-//
-function drawScene(gl, programInfo, buffers) {
-  gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
-  gl.clearDepth(1.0); // Clear everything
-  gl.enable(gl.DEPTH_TEST); // Enable depth testing
-  gl.depthFunc(gl.LEQUAL); // Near things obscure far things
-
-  // Clear the canvas before we start drawing on it.
-
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  // Create a perspective matrix, a special matrix that is
-  // used to simulate the distortion of perspective in a camera.
-  // Our field of view is 45 degrees, with a width/height
-  // ratio that matches the display size of the canvas
-  // and we only want to see objects between 0.1 units
-  // and 100 units away from the camera.
-
-  const fieldOfView = (45 * Math.PI) / 180; // in radians
-  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-  const zNear = 0.1;
-  const zFar = 100.0;
-  const projectionMatrix = mat4.create();
-
-  // note: glmatrix.js always has the first argument
-  // as the destination to receive the result.
-  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
-
-  // Set the drawing position to the "identity" point, which is
-  // the center of the scene.
-  const modelViewMatrix = mat4.create();
-
-  // Now move the drawing position a bit to where we want to
-  // start drawing the square.
-
-  mat4.translate(
-    modelViewMatrix, // destination matrix
-    modelViewMatrix, // matrix to translate
-    [-0.0, 0.0, -6.0]
-  ); // amount to translate
-
-  // Tell WebGL how to pull out the positions from the position
-  // buffer into the vertexPosition attribute.
-  {
-    const numComponents = 2;
-    const type = gl.FLOAT;
-    const normalize = false;
-    const stride = 0;
-    const offset = 0;
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-    gl.vertexAttribPointer(
-      programInfo.attribLocations.vertexPosition,
-      numComponents,
-      type,
-      normalize,
-      stride,
-      offset
-    );
-    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
-  }
-
-  // Tell WebGL to use our program when drawing
-
-  gl.useProgram(programInfo.program);
-
-  // Set the shader uniforms
-
-  gl.uniformMatrix4fv(
-    programInfo.uniformLocations.projectionMatrix,
-    false,
-    projectionMatrix
-  );
-  gl.uniformMatrix4fv(
-    programInfo.uniformLocations.modelViewMatrix,
-    false,
-    modelViewMatrix
-  );
-
-  {
-    const offset = 0;
-    const vertexCount = 4;
-    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
-  }
 }
 
 function main() {
@@ -134,30 +45,56 @@ function main() {
     return;
   }
 
+  const vs = `
+    attribute vec2 pos;
+    void main(){
+      gl_Position = vec4(pos,0,1);
+    }
+  `;
+
+  const fs = `
+    precision mediump float;
+    uniform vec4 u_color;
+    void main(){
+      gl_FragColor = u_color;
+    }
+  `;
+
   // 着色器程序
-  const shaderProgram = createProgram(gl, square_v, square_f);
+  const shaderProgram = createProgram(gl, vs, fs);
+  gl.useProgram(shaderProgram);
 
   // 获取着色器中变量的位置
   const programInfo = {
     program: shaderProgram,
     attribLocations: {
-      vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition")
-    },
-    uniformLocations: {
-      projectionMatrix: gl.getUniformLocation(
-        shaderProgram,
-        "uProjectionMatrix"
-      ),
-      modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix")
+      vertexPosAttrib: gl.getAttribLocation(shaderProgram, "pos")
     }
   };
 
-  // Here's where we call the routine that builds all the
-  // objects we'll be drawing.
-  const buffers = initBuffers(gl);
+  initBuffers(gl);
 
-  // Draw the scene
-  drawScene(gl, programInfo, buffers);
+  gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosAttrib);
+  gl.vertexAttribPointer(
+    programInfo.attribLocations.vertexPosAttrib,
+    2,
+    gl.FLOAT,
+    false,
+    0,
+    0
+  );
+
+  const u_color = gl.getUniformLocation(shaderProgram, "u_color");
+
+  gl.uniform4f(u_color, 1, 0, 0, 1);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+  const progress = document.getElementById("progress");
+  progress.addEventListener("change", e => {
+    const sliderValue = e.target.value;
+    gl.uniform4f(u_color, sliderValue / 100, 0, 0, 1);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+  });
 }
 
 main();
